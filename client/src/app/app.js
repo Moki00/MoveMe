86b13860f9f9b277c93a71864669b9e6a8e79e13
moki929 <moking929@gmail.com>
2020-09-24 16:54:04 -0400

    9-23 work

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -2,14 +2,18 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import { NavBar } from '../components'
+import './app.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
     return (
+        <div className='app'>
+
         <Router>
-            <NavBar />
+            <NavBar/>
         </Router>
+        </div>
     )
 }
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,10 +4,6 @@ import styled from 'styled-components'
 import Logo from './Logo'
 import Links from './Links'
 
-const Container = styled.div.attrs({
-    className: 'container',
-})``
-
 const Nav = styled.nav.attrs({
     className: 'navbar navbar-expand-lg navbar-dark bg-dark',
 })`
@@ -17,12 +13,12 @@ const Nav = styled.nav.attrs({
 class NavBar extends Component {
     render() {
         return (
-            <Container>
+
                 <Nav>
                     <Logo />
                     <Links />
                 </Nav>
-            </Container>
+
         )
     }
 }
