ecf642f39e63f67c1e99c85f3b7c6f62bbc7647c
moki929 <moking929@gmail.com>
2020-09-27 19:33:25 -0400

    working on SearchBar

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,8 @@
-import Links from './Links'
-import Logo from './Logo'
-import NavBar from './NavBar'
-import RandomB from './RandomB'
-import Footer from './Footer'
+import Links from "./Links";
+import Logo from "./Logo";
+import NavBar from "./NavBar";
+import RandomB from "./RandomB";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
 
-export { Links, Logo, NavBar, RandomB, Footer }
+export { Links, Logo, NavBar, RandomB, Footer, SearchBar };
