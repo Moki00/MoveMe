0c06891fa5adef55ef059aca1b99b70fa614ced2
dancingCamel <robert.brindley.morgan+github@gmail.com>
2020-09-27 17:35:37 -0400

    Created Quote Navigation Area

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,8 @@
-import Links from './Links'
-import Logo from './Logo'
-import NavBar from './NavBar'
-import RandomB from './RandomB'
-import Footer from './Footer'
+import Links from "./Links";
+import Logo from "./Logo";
+import NavBar from "./NavBar";
+import RandomB from "./RandomB";
+import Footer from "./Footer";
+import QuoteNavArea from "./QuoteNavArea";
 
-export { Links, Logo, NavBar, RandomB, Footer }
+export { Links, Logo, NavBar, RandomB, Footer, QuoteNavArea };
