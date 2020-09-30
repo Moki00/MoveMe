a9fb6284a1acdeb3ce605b4bb70622f9e7a8b965
moki929 <moking929@gmail.com>
2020-09-30 17:34:13 -0400

    added font awesome

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { NavBar, SocialIcons} from "../components";
+import { NavBar, SocialIcons } from "../components";
 import Create from "../pages/Create";
 import Home from "../pages/Home";
 import "./app.css";
@@ -9,20 +9,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "jquery/dist/jquery.js";
 import "popper.js/dist/popper";
 import "bootstrap/dist/js/bootstrap";
+import "font-awesome/css/font-awesome.css";
 
 const App = () => {
-    return (
-        <div className="app">
-            <Router>
-                <NavBar />
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/create" component={Create} />
-                </Switch>      
-                <SocialIcons />
-            </Router>            
-        </div>
-    );
+  return (
+    <div className="app">
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/create" component={Create} />
+        </Switch>
+        <SocialIcons />
+      </Router>
+    </div>
+  );
 };
 
 export default App;
