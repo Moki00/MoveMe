eeb7314ca8dfa9a05542dd255cc0aa87880e741e
dancingCamel <robert.brindley.morgan+github@gmail.com>
2020-10-01 18:11:03 -0400

    Added untracked files

---
+++ client/src/components/LeftArrowButton.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+import LeftArrow from "./LeftArrow";
+
+const Button = styled.button.attrs({
+    className: "btn btn-warning text-danger",
+})``;
+
+const LeftArrowButton = () => {
+    return (
+        <Button>
+            <LeftArrow />
+        </Button>
+    );
+};
+
+export default LeftArrowButton;
