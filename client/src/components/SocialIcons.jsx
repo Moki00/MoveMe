8fbca4ce5754697ab1e3f584fb985cab510731ab
moki929 <moking929@gmail.com>
2020-10-01 14:21:30 -0400

    just one file

--- client/src/components/SocialIcons.jsx
+++ client/src/components/SocialIcons.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import insta from'../images/instagram.png';
-import facebook from'../images/facebook.png';
-import twitter from'../images/twitter.png';
-import whatsapp from'../images/whatsapp.png';
-import './styles/social.css';
+import insta from "../images/instagram.png";
+import facebook from "../images/facebook.png";
+import twitter from "../images/twitter.png";
+import whatsapp from "../images/whatsapp.png";
+import "./styles/social.css";
 
-const SocialIcons = () =>{
-	return(
- <div className="iconspanel">
-<a href=""><img src={insta} className="fa" alt="" /></a>
-<a href=""><img src={facebook} className="fa" alt="" /></a>
-<a href=""><img src={twitter} className="fa" alt="" /></a>
-<a href=""><img src={whatsapp} className="fa" alt="" /></a>
- </div>
- );
+const SocialIcons = () => {
+  return (
+    <div className="iconspanel">
+      <a href="">
+        <img src={insta} className="fa" alt="" />
+      </a>
+      <a href="">
+        <img src={facebook} className="fa" alt="" />
+      </a>
+      <a href="">
+        <img src={twitter} className="fa" alt="" />
+      </a>
+      <a href="">
+        <img src={whatsapp} className="fa" alt="" />
+      </a>
+    </div>
+  );
 };
+
+// to remove
 
 export default SocialIcons;
