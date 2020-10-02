936c8a4c88d081a85dfdaf6b6dfd901dea5cdc6b
moki929 <moking929@gmail.com>
2020-10-02 13:45:55 -0400

    last for 10-2

--- client/src/components/ShareFacebook.jsx
+++ client/src/components/ShareFacebook.jsx
@@ -6,6 +6,24 @@ const Button = styled.button.attrs({
     type: "button",
 })``;
 
+const Text = styled.div.attrs({
+    className: "font-weight-bold shareText",
+})``;
+
+<!-- Your share button code -->
+<div class="fb-share-button" 
+data-href="https://www.your-domain.com/your-page.html" 
+data-layout="button_count">
+</div>
+
+const ShareFacebook = () => {
+    return (
+        <Button data-toggle="modal" data-target="#shareModal">
+            <Text>SHARE</Text>
+        </Button>
+    );
+};
+
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
 var js, fjs = d.getElementsByTagName(s)[0];
@@ -14,11 +32,5 @@ js = d.createElement(s); js.id = id;
 js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
 fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your share button code -->
-<div class="fb-share-button" 
-data-href="https://www.your-domain.com/your-page.html" 
-data-layout="button_count">
-</div>
 
 export default ShareFacebook;
