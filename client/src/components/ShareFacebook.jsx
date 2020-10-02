c4b531dbb8a1fec182362c5fc1f91623f99dd198
moki929 <moking929@gmail.com>
2020-10-02 13:40:28 -0400

    3rd

--- client/src/components/ShareFacebook.jsx
+++ client/src/components/ShareFacebook.jsx
@@ -1,3 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button.attrs({
+    className: "btn btn-lg btn-warning text-danger shareBtn w-75",
+    type: "button",
+})``;
+
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
 var js, fjs = d.getElementsByTagName(s)[0];
@@ -13,3 +21,4 @@ data-href="https://www.your-domain.com/your-page.html"
 data-layout="button_count">
 </div>
 
+export default ShareFacebook;
