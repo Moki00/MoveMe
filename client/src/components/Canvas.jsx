ecb798e52154e9c40a9e09be420d4764f6ab932c
dancingCamel <robert.brindley.morgan+github@gmail.com>
2020-10-03 19:04:17 -0400

    Added canvas image and text updating

--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -2,9 +2,38 @@ import React, { useRef, useEffect, useState } from "react";
 import background from "../images/blossom.jpg"; // need to get this from state passed from app.js
 import styled from "styled-components";
 
-const CanvasElement = styled.canvas.attrs({})`
+const CanvasElement = styled.canvas.attrs({ className: "w-100" })`
     border: 1px solid black;
 `;
+
+function wrapText(context, text, x, y, maxWidth, lineHeight) {
+    const words = text.split(" ");
+    let line = "";
+    let lines = [];
+
+    // split sentence into lines less than certain width
+    for (var n = 0; n < words.length; n++) {
+        var testLine = line + words[n] + " ";
+        var metrics = context.measureText(testLine);
+        var testWidth = metrics.width;
+        if (testWidth > maxWidth && n > 0) {
+            lines.push(line);
+            line = words[n] + " ";
+        } else {
+            line = testLine;
+        }
+    }
+
+    // push the left over line
+    lines.push(line);
+
+    // centre vertically
+    let offset = y - lineHeight * (lines.length / 2) + lineHeight / 2;
+    lines.forEach((element) => {
+        context.fillText(element, x, offset);
+        offset += lineHeight;
+    });
+}
 
 const Canvas = () => {
     // we use a ref to access the canvas' DOM node
@@ -12,39 +41,86 @@ const Canvas = () => {
 
     // Need to use state here for url to image and quote. Move it up to app.js later
     const [imgUrl, setImgUrl] = useState();
+    const [text, setText] = useState("Quote here");
+    const [font, setFont] = useState("Ariel");
+    const [fontSize, setFontSize] = useState(18);
+    const [fontWeight, setFontWeight] = useState("bold");
+    const [textColor, setTextColor] = useState("black");
 
+    const clickCanvas = () => {
+        // remove this for production
+        console.log("clicked canvas");
+        setText(
+            "Just some example text but what happens if it gets really long? But what happens if it gets really really really long?"
+        );
+        // setImgUrl(background);
+    };
+
+    // handle changing image
     useEffect(() => {
         const context = canvasRef.current.getContext("2d");
-        // Need to do all drawing here
-        context.fillStyle = "black";
-        context.font = "bold 18px Arial";
-
+        // clear old image
+        context.clearRect(
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+        );
         let base_image = new Image();
-        base_image.src = background;
-        base_image.height = canvasRef.current.height;
+        base_image.src = imgUrl;
 
         base_image.onload = function () {
-            // need to wait for image to load before drawing otherwise can't find it
-            context.drawImage(base_image, 0, 0);
-            context.fillText(
-                "Text",
-                canvasRef.current.width / 2 - 17,
-                canvasRef.current.height / 2 + 8
-            );
-            // context.clearRect(
-            //     0,
-            //     0,
-            //     canvasRef.current.width,
-            //     canvasRef.current.height
-            // );
+            // set width of canvas to fixed value.
+            // set height of canvas to ratio determined by base_image width/height ratio
+            const ratio = base_image.height / base_image.width;
+            canvasRef.current.height = canvasRef.current.width * ratio;
 
-            // NOTE: Clear rect deletes background, too. need to have two canvases
+            // set size of image to match new canvas size
+            context.drawImage(
+                base_image,
+                0,
+                0,
+                canvasRef.current.width,
+                canvasRef.current.height
+            );
         };
-    }, [canvasRef]);
+    }, [imgUrl]);
+
+    // handle changing text
+    useEffect(() => {
+        const context = canvasRef.current.getContext("2d");
+        context.clearRect(
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+        );
+        context.fillStyle = textColor;
+        context.font = `${fontWeight} ${fontSize}px ${font}`;
+        context.textAlign = "center";
+
+        wrapText(
+            context,
+            text,
+            canvasRef.current.width / 2,
+            canvasRef.current.height / 2,
+            canvasRef.current.width - 10,
+            fontSize
+        );
+    }, [text, font, fontSize, fontWeight, textColor]);
 
     return (
         <div>
-            <CanvasElement ref={canvasRef} />
+            <CanvasElement
+                ref={canvasRef}
+                imgUrl={imgUrl}
+                text={text}
+                font={font}
+                fontSize={fontSize}
+                fontWeight={fontWeight}
+                textColor={textColor}
+                onClick={clickCanvas}
+            />
         </div>
     );
 };
