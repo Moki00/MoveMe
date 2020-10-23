a4984bf30be5a31e6af88a4cd5cc93b66e736e0c
moki929 <moking929@gmail.com>
2020-10-23 09:15:33 -0400

    setting up backend with MongoDB

--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const app = express()
-const apiPort = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
+const db = require("./db");
+const movieRouter = require("./routes/move-router");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const app = express();
+const apiPort = 3000;
 
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(bodyParser.json());
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.use("/api", movieRouter);
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
