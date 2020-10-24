22dc4fcfc67a3c4b3c57185809159337223a8df6
dancingCamel <robert.brindley.morgan+github@gmail.com>
2020-10-24 18:21:28 -0400

    Added image resize functionality

--- client/src/pages/CardGet.jsx
+++ client/src/pages/CardGet.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { CreateLinkButton } from "../components";
 import api from "../api";
 import styled from "styled-components";
@@ -14,7 +14,6 @@ const Img = styled.img.attrs({
     className: "img",
 })`
     border: 1px solid black;
-    height: 50vh;
 `;
 
 const ImgWrapper = styled.div.attrs({
@@ -26,14 +25,52 @@ const ButtonWrapper = styled.div.attrs({
 })``;
 
 const CardGet = ({ match }) => {
+    const imgRef = useRef(null);
     const [img, setImg] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const [height, setHeight] = useState("auto");
+    const [width, setWidth] = useState("95%");
 
     React.componentDidMount = async () => {
+        // not sure this function gets fired
         setIsLoading(true);
+        console.log("mounted");
 
         await api.getCard(match.params.id).then((card) => {
-            setImg(card.data.data); // need to check this according to return from db after mongo set up
+            // check if mobile device
+            console.log(card);
+            const src = "data:image/png;base64," + card.data.data; // need to check this is the base64
+            let isMobile = window.matchMedia(
+                "only screen and (max-width: 992px)"
+            ).matches;
+            if (isMobile) {
+                console.log("mobile device");
+                setWidth("95%");
+            } else {
+                // get img and make Img object
+
+                const image = new Image();
+
+                image.src = src;
+                image.onload = function () {
+                    // if ratio > 1 then portrait (or square)
+                    const ratio = image.height / image.width;
+
+                    // set fixed width of landscape and height of portrait
+                    if (ratio > 1) {
+                        console.log("portrait/square");
+                        setHeight("70%"); // not sure % works for height. may need to use css and ref
+                        setWidth("auto");
+                    } else {
+                        console.log("landscape");
+                        setWidth("60%");
+                        setHeight("auto");
+                    }
+                };
+            }
+
+            // set img
+            setImg(src); // need to check this according to return from db after mongo set up
         });
     };
 
@@ -41,7 +78,13 @@ const CardGet = ({ match }) => {
         <ViewPageWrapper>
             <div className="row">
                 <ImgWrapper>
-                    <Img src={img} alt="A Motivational Poster from a Friend" />
+                    <Img
+                        src={img}
+                        alt="A Motivational Poster from a Friend"
+                        ref={imgRef}
+                        width={width}
+                        height={height}
+                    />
                 </ImgWrapper>
                 {/* Need to have an img in centre with create button below. Img should have max width */}
                 <ButtonWrapper>
